e3399e5c2aa2d2f4de6f0e299199b66391bf327b
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-01-10 13:56:13 +0330

    setup v0.1 of Login component

--- messenger/src/Components/Login.js
+++ messenger/src/Components/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Style from '../Styles/Login.module.css';
+import gIcon from '../Images/google-icon.png'
 
 const Login = () => {
     return (
         <>
-            <h1 className={Style.title}>Login</h1>
+            <h1 id={Style.title}>Welcome to My Messenger</h1>
+            <div className={Style.loginBox}>
+                <img src={gIcon} alt='Google Icon' />
+                <button className='pure-button'>Sign In With Google
+                </button>
+            </div>
+            <span id={Style.developer}>created with ♥️ by torabiika</span>
         </>
     );
 };
