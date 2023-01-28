01c80578d41d14f2a425d0bc7a229f307f5d01a6
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-01-28 22:20:31 +0330

    setup Login Component based on ui design

--- messenger/src/Components/Login.js
+++ messenger/src/Components/Login.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import Style from '../Styles/Login.module.css';
-import gIcon from '../Images/google-icon.png'
+import googleIcon from '../Images/google-icon.png';
+import wallpaper from '../Images/login-wallpaper.jpg';
+import plotLogo1 from '../Images/Plot-logo-1.png';
+import plotLogo2 from '../Images/Plot-logo-2.png';
 
 const Login = () => {
     return (
         <>
-            <h1 id={Style.title}>Welcome to My Messenger</h1>
-            <div className={Style.loginBox}>
-                <img src={gIcon} alt='Google Icon' />
-                <button className='pure-button'>Sign In With Google
-                </button>
+            <div className={Style.container}>
+                <div className={Style.loginBox}>
+                    <div id={Style.logo}>
+                        <img className={Style.icon} src={plotLogo1} alt='Plot Logo' />
+                    </div>
+
+                    <div id={Style.welcome}>
+                        <h1>welcome to</h1>
+                        <img className={Style.icon} src={plotLogo2} alt='Plot Logo' />
+                        <h1>Sign In to Continue</h1>
+                        <span>don't you already hava an account? <b>create one!</b></span>
+                    </div>
+
+                    <div id={Style.login}>
+                        <form>
+                            <label for='emailInput'>Email</label>
+                            <br />
+                            <input id='emailInput' type='email' placeholder='YourEmail@gmail.com' />
+                            <br />
+                            <label for='passInput'>Password</label>
+                            <br />
+                            <input id='passInput' type='password' />
+                            <br />
+                            <span> Forgot Password? </span>
+                            <input id='subInput' type='submit' value='Sign In' />
+                        </form>
+                    </div>
+
+                    <div id={Style.signup}>
+                        <button>
+                            <img className={Style.icon} src={googleIcon} />
+                            Sign In with Google
+                        </button>
+                    </div>
+                </div>
+
+                <img src={wallpaper} className={Style.loginWallpaper}>
+                </img>
             </div>
-            <span id={Style.developer}>created with ♥️ by torabiika</span>
         </>
     );
 };
