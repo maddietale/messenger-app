a4aac88af4af40bd9290ace3055177b3c02cf142
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-01-29 23:43:04 +0330

    Frame v0.1

--- messenger/src/Components/Login.js
+++ messenger/src/Components/Login.js
@@ -1,51 +1,32 @@
 import React from 'react';
 import Style from '../Styles/Login.module.css';
-import googleIcon from '../Images/google-icon.png';
-import wallpaper from '../Images/login-wallpaper.png';
-import plotLogo1 from '../Images/Plot-logo-1.png';
-import plotLogo2 from '../Images/Plot-logo-2.png';
+import wallpaper from '../Images/login-wallpaper.jpeg';
+import frameLogo1 from '../Images/Plot-logo-1.png';
 
 const Login = () => {
     return (
         <>
             <div className={Style.container}>
-                <div className={Style.loginBox}>
+
+                <div className={Style.box}>
                     <div id={Style.logo}>
-                        <img className={Style.icon} src={plotLogo1} alt='Plot Logo' />
+                        <img src={frameLogo1} alt='Plot Logo' />
                     </div>
-
-                    <div id={Style.welcome}>
-                        <h1>welcome to</h1>
-                        <img className={Style.icon} src={plotLogo2} alt='Plot Logo' />
-                        <h1>Sign In to Continue</h1>
-                        <span>don't you already hava an account? <b>create one!</b></span>
-                    </div>
-
                     <div id={Style.login}>
-                        <form className='pure-form'>
-                            <label for='emailInput'>Email</label>
-                            <br />
-                            <input id='emailInput' type='email' placeholder='YourEmail@gmail.com' />
-                            <br />
-                            <label for='passInput'>Password</label>
-                            <br />
-                            <input id='passInput' type='password' />
-                            <br />
-                            <span> Forgot Password? </span>
-                            <input className='pure-button' id='subInput' type='submit' value='Sign In' />
-                        </form>
-                    </div>
-
-                    <div id={Style.signup}>
+                        <h1>
+                            Welcome to "Frame"
+                        </h1>
                         <button className='pure-button'>
-                            <img className={Style.icon} src={googleIcon} alt='Google Logo' />
+                        <i class="fa fa-google fa-m" />
                             Sign In with Google
                         </button>
                     </div>
+                    <span>made with ♥️ by torabiika</span>
                 </div>
 
-                <img src={wallpaper} className={Style.loginWallpaper} alt='Wallpaper'>
+                <img src={wallpaper} className={Style.wallpaper} alt='Wallpaper'>
                 </img>
+
             </div>
         </>
     );
